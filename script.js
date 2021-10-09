1a5cee13b92e445acd4998190b462807c449212c
BAKIALAKSHMI GD <bakialakshmi.g.d@gmail.com>
2021-10-09 18:38:22 +0530

    Day-19

---
+++ script.js
@@ -0,0 +1,55 @@
+
+function function1() {
+    var node = document.createElement("p");
+    var textnode = document.createTextNode("Pg:1");
+    node.appendChild(textnode);
+    document.getElementById("number-active").appendChild(node);
+  }
+ function function2() {
+   var node = document.createElement("p");
+   var textnode = document.createTextNode("Pg:2");
+   node.appendChild(textnode);
+   document.getElementById("number2").appendChild(node);
+ }
+ function function3() {
+    var node = document.createElement("p");
+    var textnode = document.createTextNode("Pg:3");
+    node.appendChild(textnode);
+    document.getElementById("number3").appendChild(node);
+ }
+ function function4() {
+    var node = document.createElement("p");
+    var textnode = document.createTextNode("Pg:4");
+    node.appendChild(textnode);
+    document.getElementById("number4").appendChild(node);
+ }
+ function function5() {
+    var node = document.createElement("p");
+    var textnode = document.createTextNode("Pg:5");
+    node.appendChild(textnode);
+    document.getElementById("number5").appendChild(node);
+ }
+ function function6() {
+    var node = document.createElement("p");
+    var textnode = document.createTextNode("Pg:6");
+    node.appendChild(textnode);
+    document.getElementById("number6").appendChild(node);
+ }
+ function function7() {
+    var node = document.createElement("p");
+    var textnode = document.createTextNode("Pg:7");
+    node.appendChild(textnode);
+    document.getElementById("number7").appendChild(node);
+ }
+ function function8() {
+    var node = document.createElement("p");
+    var textnode = document.createTextNode("Pg:8");
+    node.appendChild(textnode);
+    document.getElementById("number8").appendChild(node);
+ }
+ function function9() {
+    var node = document.createElement("p");
+    var textnode = document.createTextNode("Pg:9");
+    node.appendChild(textnode);
+    document.getElementById("number9").appendChild(node);
+ }
